0cc99cfef182243d050e50ea44907021690e9ec0
Fabrice <57037980+Fabricefabfab@users.noreply.github.com>
2022-10-30 12:49:59 +0200

    Update conn.php

--- Controller/conn.php
+++ Controller/conn.php
@@ -1,6 +1,6 @@
 <?php
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "shoppn";
+$db_host = "us-cdbr-east-06.cleardb.net";
+$db_user = "ba859c13681be6";
+$db_password = "f3f3bcdf";
+$db_name = "heroku_e56909acb3436a0";
 ?>
