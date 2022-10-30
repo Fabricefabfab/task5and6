a504d9b3e1d742fe473f2676e78ab451c5568ab8
Fabrice <57037980+Fabricefabfab@users.noreply.github.com>
2022-10-30 12:48:52 +0200

    Update database_credential.php

--- Controller/database_credential.php
+++ Controller/database_credential.php
@@ -1,7 +1,7 @@
 <?php
 
-define("servename","localhost");
-define("username","root");
-define("password","");
-define("db","shoppn");
+define("servename","us-cdbr-east-06.cleardb.net");
+define("username","ba859c13681be6");
+define("password","f3f3bcdf");
+define("db","heroku_e56909acb3436a0");
 ?>
